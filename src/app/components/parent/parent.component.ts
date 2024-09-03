ca0046ed98dd0f5a8930fe6429c0b800f93a1a3e
Muso Verda <gearmobile@gmail.com>
2024-09-03 17:31:52 +0300

    feat: :sparkles: Add slots

--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'pj-parent',
   standalone: true,
   imports: [],
   templateUrl: './parent.component.html',
-  styleUrl: './parent.component.scss'
+  styleUrl: './parent.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ParentComponent {
-
-}
+export class ParentComponent {}
