c947646e32b6a1c601bc4fe3016f5bb2f5e53a7b
Muso Verda <gearmobile@gmail.com>
2024-09-03 17:12:05 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pj-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'content-projection';
+}
